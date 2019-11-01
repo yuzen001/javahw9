4cc94e1590ab1ef54059c8721346afa1d75c6fa9
yuzen001 <jamapaice1668@gmail.com>
2019-11-01 14:53:53 +0800

    Initial commit

---
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -0,0 +1,18 @@
+package com.example.lib;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+
+        for(int i = 1 ; i <=9 ;i++){
+            for(int j = 1;j<=9 ;j++){
+                System.out.print(i+"X"+j +"="+ i*j+"\t");
+            }
+
+            System.out.println();
+
+        }
+
+    }
+    
+}
